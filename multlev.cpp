806d1625f8a4a210bf8ca247f62a36bff727dcc0
11249a369-sudo <11249a369@kanchiuniv.ac.in>
2025-11-05 22:06:38 -0800

    Add files via upload

---
+++ multlev.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+class Animal {
+public:
+    void eat() {
+        cout << "Animals can eat" << endl;
+    }
+};
+class Mammal : public Animal {
+public:
+    void walk() {
+        cout << "Mammals can walk" << endl;
+    }
+};
+class Dog : public Mammal {
+public:
+    void bark() {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main() {
+    Dog d;
+    d.eat();   
+    d.walk();  
+    d.bark();  
+    return 0;
+}
